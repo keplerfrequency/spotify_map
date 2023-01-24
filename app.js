a12a30cf951f8a5cb3afccd4b9e4c9994924323d
keplerfrequency <keplerfrequency@gmail.com>
2023-01-24 17:37:33 +0100

    Clean up

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-// This fcuntion creates the plalist cards for every playlist
-  function displayPlaylists(id){
-    fetch('playlist.json')
-    .then(response => response.json())
-    .then(data => {
-      data.countries.forEach(country => {
-        country.playlists.forEach(playlist => {
-          const box = document.createElement('li');
-          box.setAttribute("class", "playlist_card")
-          box.setAttribute("id",country.country);
-  
-          const a = document.createElement('a');
-          a.target = "_blank";
-          a.rel = "noopener noreferrer";
-  
-          const img = document.createElement('img');
-          img.setAttribute("class", "playlist-img");
-          img.src = playlist.img;
-          img.setAttribute("data-href", playlist.link);
-          a.appendChild(img);
-          box.appendChild(a);
-  
-          const text = document.createElement('p');
-          text.innerText = playlist.name;
-          const text1 = document.createElement('p1');
-          text1.innerText = playlist.playlist_by;
-          box.appendChild(text);
-          box.appendChild(text1);
-          
-          box.dataset.playlistBy = playlist.playlist_by;
-          
-          const div = document.createElement('div');
-          div.textContent = playlist.description;
-          div.setAttribute("class", "playlist_description")
-          box.appendChild(div)
+//Load the playlists when the window loads 
+window.onload = function() {
+  displayPlaylists("playlists");
+}  
+// This function creates the plalist cards for every playlist
+function displayPlaylists(id){
+  fetch('playlist.json')
+  .then(response => response.json())
+  .then(data => {
+    data.countries.forEach(country => {
+      country.playlists.forEach(playlist => {
+        const box = document.createElement('li');
+        box.setAttribute("class", "playlist_card")
+        box.setAttribute("id",country.country);
 
-          document.getElementById(id).appendChild(box);
-        });
+        const a = document.createElement('a');
+        a.target = "_blank";
+        a.rel = "noopener noreferrer";
+
+        const img = document.createElement('img');
+        img.setAttribute("class", "playlist-img");
+        img.src = playlist.img;
+        img.setAttribute("data-href", playlist.link);
+        a.appendChild(img);
+        box.appendChild(a);
+
+        const text = document.createElement('p');
+        text.innerText = playlist.name;
+        const text1 = document.createElement('p1');
+        text1.innerText = playlist.playlist_by;
+        box.appendChild(text);
+        box.appendChild(text1);
+        
+        box.dataset.playlistBy = playlist.playlist_by;
+        
+        const div = document.createElement('div');
+        div.textContent = playlist.description;
+        div.setAttribute("class", "playlist_description")
+        box.appendChild(div)
+
+        document.getElementById(id).appendChild(box);
       });
     });
-  }
+  });
+}
 
 //This function will toggle the spotify playlists
 document.addEventListener('DOMContentLoaded', function(){
@@ -57,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function(){
 document.addEventListener('DOMContentLoaded', function(){
   const toggleButton = document.getElementById("toggle-button-user").querySelector('input');
   const playlistByValue = "Spotify";
-
+  
   toggleButton.addEventListener("change", function() {
     const elements = document.querySelectorAll(`[data-playlist-by]:not([data-playlist-by='${playlistByValue}'])`);
     elements.forEach(function(element) {

6e74f5f88d1ad2da81074c77a3b205434cd88e1b
keplerfrequency <keplerfrequency@gmail.com>
2023-01-24 23:54:02 +0100

    Making things safe
    removing client id/secret

--- get_playlists.py
+++ get_playlists.py
@@ -7,9 +7,8 @@ import os
 import shutil
 import math
 
-#Get this here: https://developer.spotify.com/dashboard/applications
-CLIENT_ID = '948c4e2f99254fcdbf7e8f9779da03b6'
-CLIENT_SECRET = '31711ce22be54da09b8aabe950c42b09'
+import config.py
+
 #Output JSON is where the response from spotify is temporarily stored and then removed 
 OUTPUT_JSON = "test_output_playlists.json"
 #Playlist.json file is where all the playlists are stored. the temp is a temp file that then overwrites the other one
@@ -21,7 +20,7 @@ PLAYLIST_METADATA = 6
 #Do a search of the playslists
 def get_playlists(query, type, market, offset):
 
-    spotify = spotipy.Spotify(requests_timeout=10, client_credentials_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, ))
+    spotify = spotipy.Spotify(requests_timeout=10, client_credentials_manager=SpotifyClientCredentials(client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, ))
 
     valid_markets = ["AD","AE","AG","AL","AM","AO","AR","AT","AU","AZ","BA","BB","BD","BE","BF","BG","BH","BI","BJ","BN","BO","BR","BS","BT","BW","BY","BZ","CA","CD","CG","CH","CI","CL","CM","CO","CR","CV","CW","CY","CZ","DE","DJ","DK","DM","DO","DZ","EC","EE","EG","ES","ET","FI","FJ","FM","FR","GA","GB","GD","GE","GH","GM","GN","GQ","GR","GT","GW","GY","HK","HN","HR","HT","HU","ID","IE","IL","IN","IQ","IS","IT","JM","JO","JP","KE","KG","KH","KI","KM","KN","KR","KW","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","ME","MG","MH","MK","ML","MN","MO","MR","MT","MU","MV","MW","MX","MY","MZ","NA","NE","NG","NI","NL","NO","NP","NR","NZ","OM","PA","PE","PG","PH","PK","PL","PS","PT","PW","PY","QA","RO","RS","RW","SA","SB","SC","SE","SG","SI","SK","SL","SM","SN","SR","ST","SV","SZ","TD","TG","TH","TJ","TL","TN","TO","TR","TT","TV","TW","TZ","UA","UG","US","UY","UZ","VC","VE","VN","VU","WS","XK","ZA","ZM","ZW"]
 

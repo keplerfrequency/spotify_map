ad259fd0fa423c0a2e13bf7b4c5c385fb9fd03d4
alexandre <keplerfrequency@gmail.com>
2023-01-27 08:50:36 +0100

    Added proper parsing of key variables

--- get_playlists_safe.py
+++ get_playlists_safe.py
@@ -7,20 +7,24 @@ import os
 import shutil
 import math
 
-import config.py
+from configparser import ConfigParser
+config=ConfigParser()
+config.read('config.ini')
+CLIENT_ID = config.get('keys', 'CLIENT_ID')
+CLIENT_SECRET = config.get('keys', 'CLIENT_SECRET')
 
 #Output JSON is where the response from spotify is temporarily stored and then removed 
 OUTPUT_JSON = "test_output_playlists.json"
 #Playlist.json file is where all the playlists are stored. the temp is a temp file that then overwrites the other one
 PLAYLIST_JSON = "playlist.json"
 TEMP_PLAYLIST_JSON = "temp_playlist.json"
-NUMBER_OF_PLAYLISTS = 250
+NUMBER_OF_PLAYLISTS = 100
 PLAYLIST_METADATA = 6
 
 #Do a search of the playslists
 def get_playlists(query, type, market, offset):
 
-    spotify = spotipy.Spotify(requests_timeout=10, client_credentials_manager=SpotifyClientCredentials(client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, ))
+    spotify = spotipy.Spotify(requests_timeout=10, client_credentials_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET, ))
 
     valid_markets = ["AD","AE","AG","AL","AM","AO","AR","AT","AU","AZ","BA","BB","BD","BE","BF","BG","BH","BI","BJ","BN","BO","BR","BS","BT","BW","BY","BZ","CA","CD","CG","CH","CI","CL","CM","CO","CR","CV","CW","CY","CZ","DE","DJ","DK","DM","DO","DZ","EC","EE","EG","ES","ET","FI","FJ","FM","FR","GA","GB","GD","GE","GH","GM","GN","GQ","GR","GT","GW","GY","HK","HN","HR","HT","HU","ID","IE","IL","IN","IQ","IS","IT","JM","JO","JP","KE","KG","KH","KI","KM","KN","KR","KW","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","ME","MG","MH","MK","ML","MN","MO","MR","MT","MU","MV","MW","MX","MY","MZ","NA","NE","NG","NI","NL","NO","NP","NR","NZ","OM","PA","PE","PG","PH","PK","PL","PS","PT","PW","PY","QA","RO","RS","RW","SA","SB","SC","SE","SG","SI","SK","SL","SM","SN","SR","ST","SV","SZ","TD","TG","TH","TJ","TL","TN","TO","TR","TT","TV","TW","TZ","UA","UG","US","UY","UZ","VC","VE","VN","VU","WS","XK","ZA","ZM","ZW"]
 
@@ -164,6 +168,8 @@ def main():
     #Countries
     europe_countries = ["albania","andorra","austria","belarus","belgium","bosnia and herzegovina","bulgaria","croatia","cyprus","czech republic","denmark","estonia","france","finland","georgia","germany","greece","hungary","iceland","ireland","san marino","italy","kosovo","latvia","liechtenstein","lithuania","luxembourg","macedonia","malta", "moldova","monaco","montenegro","netherlands","norway","poland","portugal","romania","russian federation","serbia","slovakia","slovenia","spain","sweden","switzerland","turkey","ukraine","england", "isle of man", "northern ireland", "scotland", "wales"]
     
+    europe_countries = ["portugal"]
+
     list_of_all_playlists = [None]*PLAYLIST_METADATA
     number_of_searches = math.ceil(NUMBER_OF_PLAYLISTS / 50)
 

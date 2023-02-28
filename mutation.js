e6cb9c23414189b03266f13a929ce321e7c1a425
keplerfrequency <keplerfrequency@gmail.com>
2023-02-28 17:30:30 +0100

    Fixed mutation observer. loading is nicer

--- mutation.js
+++ mutation.js
@@ -1,17 +1,15 @@
 const observer = new MutationObserver(function(mutationsList) {
-    console.log("mutation detected");
     handleLiElements()
   });
   
 const targetNode = document.getElementById('playlists');
 const config = { attributes: true, childList: true, subtree: true };
-let liElements = document.querySelectorAll('#playlists li');
 
 observer.observe(targetNode, config);
 
 function handleLiElements() {
-
-
+    
+    let liElements = document.querySelectorAll('#playlists li');
     let allHidden = true;
 
     for (let i = 0; i < liElements.length; i++) {
